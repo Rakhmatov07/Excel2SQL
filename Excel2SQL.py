4901d02aed3c7ab1dd4c8ad0ac41ffc3d53f235b
Rakhmatov07 <111786777+Rakhmatov07@users.noreply.github.com>
2022-11-16 10:41:52 +0500

    Add files via upload

---
+++ Excel2SQL.py
@@ -0,0 +1,32 @@
+import pandas
+import csv
+import sqlite3
+
+"""This project imports excel file to database"""
+excel_file = input('Enter an excel file (ex:file.xls/xlsx):')
+database = input('Create new database(ex:example.db):')
+
+
+def import_excel2sqlite(excel_file, database):
+
+    df = pandas.read_excel(excel_file, header=0)
+    connection = sqlite3.connect(database)
+    df.to_sql(
+        name='full_info',
+        con=connection,
+        if_exists='replace',
+        index=False,
+        dtype={
+            'First Name': 'txt',
+            'Last Name': 'txt',
+            'Gender': 'txt',
+            'Country': 'txt',
+            'Age': 'real',
+            'Date': 'real',
+            'Id': 'real'
+        }
+    )
+    return df
+
+
+print(import_excel2sqlite(excel_file=excel_file, database=database))
